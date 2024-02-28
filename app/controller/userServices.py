8ebc94bad4941435d3ce842195893e93a1481e59
chananon-n <91945879+chananon-n@users.noreply.github.com>
2024-02-28 21:16:42 +0700

    change file name and create func for user

--- app/controller/userServices.py
+++ app/controller/userServices.py
@@ -3,8 +3,11 @@ from app.services.databaseServices import DatabaseServices
 
 class UserServices:
         
-    def register(self):
-        pass
+    def register(self,usernameInput,emailInput, phoneInput, passwordInput):
+        #check user role
+        role = DatabaseServices.validationUserRegister(emailInput)
+        return True
+        
     
     def login(self,usernameInput, passwordInput):
         return DatabaseServices.validateUserLogin(usernameInput, passwordInput)
